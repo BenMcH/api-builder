66e912e9a630d7b9ccb26607a7a2a6d7d9a6256e
Ben McHone <benjamin.mchone@gmail.com>
2019-03-29 22:21:52 -0500

    Added react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import RuleSet from './RuleSet.js';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const App = () => {
-  return (
+const App = () => (
+  <Router>
+    <Route path="/:name" component={ruleSet} />
+  </Router>
+);
+
+const ruleSet = ({match: {params: {name}}}) => (
     <div className="App">
-      <RuleSet name="stateAbbreviations" />
+      <RuleSet name={name} />
     </div>
-  );
-}
+);
 
 export default App;
